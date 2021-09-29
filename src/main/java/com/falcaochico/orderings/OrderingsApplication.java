d5887540d0ea5573829ce9ce1a5865a680d5609e
Francisco Falcão <falcaofranciscojr@gmail.com>
2021-09-29 20:06:07 -0300

    First commit

---
+++ src/main/java/com/falcaochico/orderings/OrderingsApplication.java
@@ -0,0 +1,13 @@
+package com.falcaochico.orderings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falcaochico/orderings/OrderingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falcaochico.orderings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
